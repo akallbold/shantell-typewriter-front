10851618099a0058e114f9e147cbf48d2d1ba69c
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2023-05-09 16:47:44 -0700

    cleanup

--- src/InputPanel.js
+++ src/InputPanel.js
@@ -4,42 +4,36 @@ import { Button, IconButton, Box, TextField } from "@mui/material";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 
 function InputPanel(props) {
-  const { input, setInput, setError, loading, setLoading } = props;
+  const { input, setInput, setError, setLoading } = props;
   const [url, setUrl] = useState("");
-
   const saveTextToDatabase = async () => {
     setLoading(true);
     const uuid = Math.random().toString(36).substring(7);
     const body = JSON.stringify({ text: input, id: uuid });
-    try {
-      fetch("/.netlify/functions/writeText", {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-        body: JSON.stringify(body),
+
+    fetch("/.netlify/functions/writeText", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(body),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setUrl(`${window.location.href}${res.id}`);
+        setLoading(false);
       })
-        .then(function (res) {
-          console.log({ res });
-          setUrl(`${window.location.href}${uuid}`);
-          setLoading(false);
-        })
-        .catch(function (res) {
-          setError(res);
-          setLoading(false);
-        });
-    } catch (err) {
-      console.log(err);
-    }
+      .catch(function (res) {
+        setError(res);
+        setLoading(false);
+      });
   };
 
-  const copyText = (t) => {
-    console.log("Copy text to clipboard: ", t);
-    navigator.clipboard.writeText(t);
+  const copyText = () => {
+    console.log("Copy text to clipboard: ", url);
+    navigator.clipboard.writeText(url);
   };
-
-  if (loading) return null;
 
   return (
     <Box sx={{ width: "100%" }}>
@@ -52,7 +46,7 @@ function InputPanel(props) {
       {/* {url && ( */}
       <>
         <h3>{url}</h3>
-        <IconButton onClick={() => copyText(url)}>
+        <IconButton onClick={copyText}>
           <ContentCopyIcon />
         </IconButton>
       </>
