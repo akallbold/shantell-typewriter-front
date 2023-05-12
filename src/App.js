87ed525bfaf184252c9da56e50ba657e056430dd
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2023-05-12 12:34:52 -0700

    added id dependency

--- src/App.js
+++ src/App.js
@@ -13,41 +13,38 @@ function App() {
   let { id } = useParams();
 
   const readText = () => {
-    if (id) {
-      setLoading(true);
-      fetch("/.netlify/functions/getText", {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-        body: JSON.stringify({ id }),
+    setLoading(true);
+    fetch("/.netlify/functions/getText", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ id }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setText(res.text);
+        setLoading(false);
       })
-        .then((res) => res.json())
-        .then((res) => {
-          setText(res.text);
-          setLoading(false);
-        })
-        .catch((res) => {
-          setError(res);
-          setLoading(false);
-        });
-    }
+      .catch((res) => {
+        setError(res);
+        setLoading(false);
+      });
   };
 
   useEffect(() => {
-    readText();
+    if (id) {
+      readText();
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [id]);
 
   if (error) console.log({ error });
 
   if (loading) return null;
   return (
-    <div
-      className="App"
-      // style={{ padding: "2rem" }}
-    >
+    <div className="App">
       <Grid2
         flexDirection="column"
         container
