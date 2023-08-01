c243648be243667c9669eee8db15f6a21498a62d
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2023-08-01 11:09:32 -0700

    CORS

--- src/InputPanel.js
+++ src/InputPanel.js
@@ -21,6 +21,7 @@ function InputPanel(props) {
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
       },
       method: "POST",
       body: JSON.stringify(body),
